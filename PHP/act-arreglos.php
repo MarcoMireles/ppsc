249e617138123d7346911e37277f511921c95087
Marco Mireles <marcomirelesvilla@gmail.com>
2022-03-24 09:52:35 -0600

    PC 2

---
+++ PHP/act-arreglos.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="act-arreglos-send.php" method="post">
+    <div class="inputNumero">
+        <label for="num1">Ingresa un numero</label>
+        <input type="number" id="num1" name="num1">
+    </div>
+
+    <div class="inputNumero">
+        <label for="num2">Ingresa un numero</label>
+        <input type="number" id="num2" name="num2">
+    </div>
+
+    <div class="inputNumero">
+        <label for="num3">Ingresa un numero</label>
+        <input type="number" id="num3" name="num3">
+    </div>
+
+    <div class="btn">
+        <button type="submit">Enviar</button>
+    </div>
+
+
+</form>
+</body>
+</html>
